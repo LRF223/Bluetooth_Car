b7f4ab62381898bea3eccb49bae5ebaecff98423
LRF223 <455492037@qq.com>
2021-03-14 13:25:02 +0800

    pwm调速

--- Hardware/Motor/motor.c
+++ Hardware/Motor/motor.c
@@ -3,11 +3,12 @@
 void MOTOR_Init(void){ //MOTOR接口初始化
 	GPIO_InitTypeDef  GPIO_InitStructure; 	
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOB|RCC_APB2Periph_GPIOC,ENABLE);       
-    GPIO_InitStructure.GPIO_Pin = Motor_L1 | Motor_L2 | Motor_R1 | Motor_R2; //选择端口号（0~15或all）                        
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8; //选择端口号（0~15或all）                        
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; //选择IO接口工作方式       
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; //设置IO接口速度（2/10/50MHz）    
 	GPIO_Init(MOTORPORT, &GPIO_InitStructure);			
 }
+
 void Go_Forward(void){
 		GPIO_WriteBit(MOTORPORT,Motor_L1,(BitAction)(0)); 
 		GPIO_WriteBit(MOTORPORT,Motor_L2,(BitAction)(1)); 
--- User/main.c
+++ User/main.c
@@ -5,6 +5,7 @@
 #include "key.h"
 #include "usart.h"
 
+#include "pwm.h"
 
 int main (void){//主程序
 	u8 a;
@@ -13,43 +14,85 @@ int main (void){//主程序
 	MOTOR_Init();//LED初始化
 	KEY_Init();//按键初始化
 	USART2_Init(9600); //串口初始化（参数是波特率）
+	
+	TIM3_PWM_Init(59999,23);
+	//设置频率为50Hz，公式为：溢出时间Tout（单位秒）=(arr+1)(psc+1)/Tclk 20MS = (59999+1)*(23+1)/72000000
+  //Tclk为通用定时器的时钟，如果APB1没有分频，则就为系统时钟，72MHZ
+  //PWM时钟频率=72000000/(59999+1)*(23+1) = 50HZ (20ms),设置自动装载值60000,预分频系数24
 
+		TIM_SetCompare3(TIM3,45000);        //改变比较值TIM3->CCR2达到调节占空比的效果
+		TIM_SetCompare4(TIM3,45000);	
+	
 	//主循环
 	while(1){
-		int b = 100;
-		int c = 50;
+		int c = 150;
 		//查询方式接收
 		if(USART_GetFlagStatus(USART2,USART_FLAG_RXNE) != RESET){  //查询串口待处理标志位
 			a =USART_ReceiveData(USART2);//读取接收到的数据
 			switch (a){
 				case '0':
 					Go_Forward();
-					delay_ms(b);
-					Stop();
+					delay_ms(c);
+					if(USART_GetFlagStatus(USART2,USART_FLAG_RXNE) == RESET)//检测到按键松开就停下
+					{	
+						Stop();							
+					}			
 					printf("%c:Go_Forward ",a); //
 					break;
+				
 				case '1':
 					Draw_Back();
-					delay_ms(b);
-					Stop();					
+					delay_ms(c);
+					if(USART_GetFlagStatus(USART2,USART_FLAG_RXNE) == RESET)
+					{	
+						Stop();							
+					}			
 					printf("%c:Draw_Back",a); //
 					break;
+				
 				case '2':
 					Turn_left();
-					delay_ms(c);
-					Stop();				
-					printf("%c:Turn_left",a); //把收到的数据发送回电脑
+					printf("%c:Turn_left",a); //把收到的数据发送回电脑			
+					delay_ms(c);				
+					if(USART_GetFlagStatus(USART2,USART_FLAG_RXNE) == RESET)
+					{	
+						Stop();							
+					}			
 					break;
+				
 				case '3':
 					Turn_right();
 					delay_ms(c);
-					Stop();				
+					if(USART_GetFlagStatus(USART2,USART_FLAG_RXNE) == RESET)
+					{	
+						Stop();							
+					}			
 					printf("%c:Turn_right ",a); //把收到的数据发送回电脑
 					break;
+				
 					case '4':
 					Stop();
 					printf("%c:Stop ",a); //把收到的数据发送回电脑
 					break;
+					
+					case '5':			
+					TIM_SetCompare3(TIM3,2000);        //改变比较值TIM3->CCR2达到调节占空比的效果
+					TIM_SetCompare4(TIM3,2000);
+					printf("%c:fast speed ",a); //把收到的数据发送回电脑
+					break;
+					
+					case '6':
+					TIM_SetCompare3(TIM3,40000);        //改变比较值TIM3->CCR2达到调节占空比的效果
+					TIM_SetCompare4(TIM3,40000);
+					printf("%c:medium speed ",a); //把收到的数据发送回电脑
+					break;
+
+					case '7':			
+					TIM_SetCompare3(TIM3,48000);        //改变比较值TIM3->CCR2达到调节占空比的效果
+					TIM_SetCompare4(TIM3,48000);
+					printf("%c:slow speed ",a); //把收到的数据发送回电脑
+					break;					
+					
 				default:
 					break;
 			}		  
